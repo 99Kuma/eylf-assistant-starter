e855591a210d2067d2318a42ec373ce4095c960a
99Kuma <87743446+99Kuma@users.noreply.github.com>
2026-06-03 21:30:41 +0800

    Add transition framework

---
+++ lib/transitionFramework.ts
@@ -0,0 +1,12 @@
+export const transitionFramework = {
+sections:[
+'Strengths and Interests',
+'Current Learning Goals',
+'Relationships and Belonging',
+'Communication and Language',
+'Physical Development',
+'Care Routines and Wellbeing',
+'Transition Supports',
+'Recommendations for Receiving Educators'
+]
+};
